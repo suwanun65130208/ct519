f7289fafaf260878bf7ceacd69464213e98da275
suwanun65130208 <suwanun65130208@users.noreply.github.com>
2023-08-06 16:26:40 +0700

    Update process_form.php

--- myweb/process_form.php
+++ myweb/process_form.php
@@ -7,28 +7,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $comment = $_POST["comment"];
 
    // Validate and sanitize the data (you can add more validation if needed)
+   
+   
 
-   // Connect to the database
-   $dbHost = "mariadb"; // Replace with your database host
-   $dbUser = "suwanun"; // Replace with your database username
-   $dbPass = "suwanun"; // Replace with your database password
-   $dbName = "mybooksuwanun"; // Replace with your database name
-
-   $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
-
+   $servername = "mariadb";
+   $username = "suwanun";
+   $password = "suwanun";
+   $database="mybooksuwanun";
+   // Create connection
+   $conn = new mysqli($servername, $username, $password,$database);
+   
    // Check connection
    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+     die("Connection failed: " . $conn->connect_error);
    }
-
-   // Insert data into the database
-   $sql = "INSERT INTO books (namebook, typebook, comment) VALUES ('$namebook', '$typebook', '$comment')";
-
-   if ($conn->query($sql) === TRUE) {
-      echo "Data inserted successfully!";
-   } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-   }
+   // Set character set to utf8mb4
+   if (!$conn->set_charset("utf8mb4")) {
+     die("Error setting character set: " . $conn->error);
+   }   
 
    // Close the database connection
    $conn->close();
